dff55c0996f3a86ad9146446cc956ee7474c2351
maheshwaran <152316758+MaheshWaran-B@users.noreply.github.com>
2026-09-05 09:04:41 +0530

    Time: 1 ms (25.83%), Space: 45 MB (61.84%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
+
+
+public class Main {
+    public static boolean uniformArray(int[] nums1) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 3, 4, 5};
+
+        boolean result = uniformArray(nums1);
+
+        System.out.println(result);
+    }
+}
